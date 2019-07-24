da75cfc595e4107a11d65ebf04e2b5babaa5b3fc
kmkzt <info.pscreator@gmail.com>
2019-07-24 22:45:54 +0900

    feat: random box example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
 extern crate piston_window;
+extern crate rand;
 
+use rand::prelude::*;
 use piston_window::*;
+// use std::io;
 
 fn main() {
+    let width = 640;
+    let height = 480;
     let mut window: PistonWindow =
-        WindowSettings::new("Breaking blocks", [640, 480])
+        WindowSettings::new("Breaking blocks", [width, height])
         .exit_on_esc(true).build().unwrap();
     while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics, _device| {
-            clear([1.0; 4], graphics);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      context.transform,
-                      graphics);
+        let rand_width = random::<f64>() * 100.0;
+        let rand_height = random::<f64>() * 100.0;
+        let rand_x = random::<f64>() * (width as f64);
+        let rand_y = random::<f64>() * (height as f64);
+
+        window.draw_2d(&event, |c, g, _device| {
+            clear([1.0; 4], g);
+            rectangle(get_rand_rgba(), // red
+                      [rand_x, rand_y, rand_width, rand_height],
+                      c.transform,
+                      g);
         });
     }
 }
+
+
+fn get_rand_rgba() -> [f32; 4] {
+    [
+        random::<f32>(),
+        random::<f32>(),
+        random::<f32>(),
+        random::<f32>()
+    ]
+}
+// fn get_keyboard_event() -> io::Result<String> {
+//     let mut input = String::new();
+
+//     io::stdin().read_line(&mut input)?;
+
+//     println!("You typed: {}", input.trim());
+//     Ok(input)
+// }
