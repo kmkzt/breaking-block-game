97c2d5d186e353f455444f00b85df58729924b0a
kmkzt <info.pscreator@gmail.com>
2019-08-12 23:34:23 +0900

    feat: multiple block

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ mod linear_node;
 use ball::Ball;
 use block::{Block, Hit};
 use controller::Controller;
-use linear_node::LinearNode;
+// use linear_node::LinearNode;
 
 fn main() {
     let width: f64 = 640.0;
@@ -23,8 +23,12 @@ fn main() {
         WindowSettings::new("Breaking blocks", [width as u32, height as u32])
         .exit_on_esc(true).build().unwrap();
 
-    let mut blocks = LinearNode::Empty;
-    blocks.add(Block::new_rand(width, height / 2.0));
+    let mut blocks = Vec::new();
+    for _ in 0..2 {
+        blocks.push(Block::new_rand(width, height));
+    }
+
+    // blocks.add(Block::new_rand(width, height / 2.0));
     let mut ball = Ball::new(width, height);
     let mut controller = Controller::new(width, height);
     while let Some(e) = window.next() {
@@ -37,30 +41,37 @@ fn main() {
         }
         
         // Bounce Block
-        match blocks {
-            LinearNode::Empty => blocks.add(Block::new_rand(width, height / 2.0)),
-            LinearNode::NonEmpty(ref mut block) => {
-                match block.node.touch(&ball) {
-                    Some(Hit::Bottom) | Some(Hit::Top) => {
-                        ball.dy *= -1.0;
-                        block.node.rand(width, height);
-                    },
-                    Some(Hit::Right) | Some(Hit::Left) => {
-                        ball.dx *= -1.0;
-                        block.node.rand(width, height);
-                    }
-                    _ => {}
+        let mut remove_index = blocks.len();
+        for (index, block) in blocks.iter_mut().enumerate() {
+            match block.touch(&ball) {
+                Some(Hit::Bottom) | Some(Hit::Top) => {
+                    ball.dy *= -1.0;
+                    // Infinity Block Mode
+                    // block.rand(width, height);
+                    remove_index = index;
+                    break;
+                },
+                Some(Hit::Right) | Some(Hit::Left) => {
+                    ball.dx *= -1.0;
+                    // Infinity Block Mode
+                    // block.rand(width, height);
+                    remove_index = index;
+                    break;
                 }
-            }   
+                _ => {}
+            }
         }
-        
+        // Remove Block 
+        if (remove_index < blocks.len()) {
+            blocks.remove(remove_index);
+        }
+    
 
         // Draw a screen.
         window.draw_2d(&e, |c, g, _device| {
             clear([1.0; 4], g);
-            match blocks {
-                LinearNode::Empty => {},
-                LinearNode::NonEmpty(ref mut block) => block.node.draw(c.transform, g)
+            for block in &blocks {
+                block.draw(c.transform, g)
             }
             ball.draw(c.transform, g);
             controller.draw(c.transform, g);
@@ -100,9 +111,8 @@ fn main() {
             let [w, h] = args.window_size;
             controller = Controller::new(w, h);
             
-            match blocks {
-                LinearNode::Empty => {},
-                LinearNode::NonEmpty(ref mut block) => block.node.rand(width, height / 2.0)
+            for block in &mut blocks {
+                block.rand(width, height / 2.0)
             }
         }
 
