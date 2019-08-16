7b0bbde8ebc1f79623f97e58afb9d5ee021f1f5a
kmkzt <info.pscreator@gmail.com>
2019-08-16 13:35:08 +0900

    feat: add status flag

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ use controller::Controller;
 use stage::Stage;
 // use linear_node::LinearNode;
 
+enum Status {
+    Stop,
+    Action,
+}
 fn main() {
     let width: f64 = 640.0;
     let height: f64 = 480.0;
@@ -30,6 +34,7 @@ fn main() {
     // blocks.add(Block::new_rand(width, height / 2.0));
     let mut ball = Ball::new(width, height);
     let mut controller = Controller::new(width, height);
+    let mut status = Status::Stop;
     while let Some(e) = window.next() {
         // Bounce Frame 
         ball.bounce_frame(width, height);
@@ -44,70 +49,65 @@ fn main() {
         }
         
         // Bounce Block
-        let mut remove_index = blocks.len();
         for (index, block) in blocks.iter_mut().enumerate() {
             match block.touch(&ball) {
                 Some(Hit::Bottom) | Some(Hit::Top) => {
                     ball.dy *= -1.0;
                     // Infinity Block Mode
                     // block.rand(width, height);
-                    remove_index = index;
+                    blocks.remove(index);
                     break;
                 },
                 Some(Hit::Right) | Some(Hit::Left) => {
                     ball.dx *= -1.0;
                     // Infinity Block Mode
                     // block.rand(width, height);
-                    remove_index = index;
+                    blocks.remove(index);
                     break;
                 }
                 _ => {}
             }
         }
-        // Remove Block 
-        if (remove_index < blocks.len()) {
-            blocks.remove(remove_index);
-        }
-    
-
-        // Draw a screen.
-        window.draw_2d(&e, |c, g, _device| {
-            clear([1.0; 4], g);
-            for block in &blocks {
-                block.draw(c.transform, g)
-            }
-            ball.draw(c.transform, g);
-            controller.draw(c.transform, g);
-        });
 
         // Coordinate change of ball.
-        ball.effect();
+        match status {
+            Status::Action => {
+                ball.effect();
+            },
+            _ => {}
+        }
+       
 
         // Mouse Event Handler
         if let Some(ref args) = e.mouse_cursor_args() {
-            let [mouse_x, _mouse_y] = *args;
+            println!("{:?}" , *args);
+            let window_size = window.size();
+            let [mouse_x, mouse_y] = *args;
+            if mouse_x < 0.1 || mouse_y < 0.1 || mouse_y > window_size.height || mouse_x > window_size.width {
+                status = Status::Stop;
+            } else {
+                status = Status::Action;
+            }
             controller.x = mouse_x;
         }
 
         // Key Event Handler
         if let Some(ref args) = e.press_args() {
-            use piston_window::Button::Keyboard;
-            
-            if *args == Keyboard(Key::Up) {
+            if *args == Button::Keyboard(Key::Up) {
                 stage.level_up(1);
                 blocks = stage.gen_blocks(width, height);
             }
-             if *args == Keyboard(Key::Down) {
+             if *args == Button::Keyboard(Key::Down) {
                 stage.level_down(1);
                 blocks = stage.gen_blocks(width, height);
             }
-            if *args == Keyboard(Key::Left) {
+            if *args == Button::Keyboard(Key::Left) {
                 controller.x -= controller.move_speed;
             }
-            if *args == Keyboard(Key::Right) {
+            if *args == Button::Keyboard(Key::Right) {
                 controller.x += controller.move_speed;
             }
-            if *args == Keyboard(Key::Space) {
+            if *args == Button::Keyboard(Key::Space) {
                 ball = Ball::new(width, height);
             }
         }
@@ -122,6 +122,20 @@ fn main() {
             }
         }
 
+        // Draw a screen.
+        window.draw_2d(&e, |c, g, _device| {
+            match status {
+                Status::Action => {
+                    clear([1.0; 4], g);
+                    for block in &blocks {
+                        block.draw(c.transform, g)
+                    }
+                    ball.draw(c.transform, g);
+                    controller.draw(c.transform, g);
+                },
+                Status::Stop => {}
+            }
+        });
     }
 }
 
